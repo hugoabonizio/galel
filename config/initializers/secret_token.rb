21a6ceb94c7bb13976716ddaea7b90348e576e02
hugoabonizio <hugo_abonizio@hotmail.com>
2013-06-07 11:09:50 -0300

    upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Galel::Application.config.secret_token = 'fe4ce76157fb23843f77cb8cbb2b32fc9ee2bce9cad345063bd643e84a0856693e316c0b58c14a5c6b23273c47fa9eef92c1a048d5b922c793af7822ad6a1a43'

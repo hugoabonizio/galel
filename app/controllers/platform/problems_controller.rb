a19459f6b8cdc14264e309f644a13a787edc70bd
hugoabonizio <hugo_abonizio@hotmail.com>
2013-06-12 11:42:06 -0300

    adicionados scaffolds sem resposta json

--- app/controllers/platform/problems_controller.rb
+++ app/controllers/platform/problems_controller.rb
@@ -1,13 +1,7 @@
 class Platform::ProblemsController < ApplicationController
   # GET /platform/problems
-  # GET /platform/problems.json
   def index
     @platform_problems = Platform::Problem.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @platform_problems }
-    end
   end
 
   # GET /platform/problems/1
@@ -39,30 +33,20 @@ class Platform::ProblemsController < ApplicationController
   end
 
   # PUT /platform/problems/1
-  # PUT /platform/problems/1.json
   def update
     @platform_problem = Platform::Problem.find(params[:id])
 
-    respond_to do |format|
-      if @platform_problem.update_attributes(params[:platform_problem])
-        format.html { redirect_to @platform_problem, notice: 'Problem was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @platform_problem.errors, status: :unprocessable_entity }
-      end
+    if @platform_problem.update_attributes(params[:platform_problem])
+      redirect_to @platform_problem, notice: 'Problem was successfully updated.'
+    else
+      redirect_to edit_platform_problems_path, notice: 'Problema na atuazacao'
     end
   end
 
   # DELETE /platform/problems/1
-  # DELETE /platform/problems/1.json
   def destroy
     @platform_problem = Platform::Problem.find(params[:id])
     @platform_problem.destroy
-
-    respond_to do |format|
-      format.html { redirect_to platform_problems_url }
-      format.json { head :no_content }
-    end
+    format.html { redirect_to platform_problems_url
   end
 end
--- app/controllers/platform/solutions_controller.rb
+++ app/controllers/platform/solutions_controller.rb
@@ -1,35 +1,17 @@
 class Platform::SolutionsController < ApplicationController
   # GET /platform/solutions
-  # GET /platform/solutions.json
   def index
     @platform_solutions = Platform::Solution.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @platform_solutions }
-    end
   end
 
   # GET /platform/solutions/1
-  # GET /platform/solutions/1.json
   def show
     @platform_solution = Platform::Solution.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @platform_solution }
-    end
   end
 
   # GET /platform/solutions/new
-  # GET /platform/solutions/new.json
   def new
     @platform_solution = Platform::Solution.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @platform_solution }
-    end
   end
 
   # GET /platform/solutions/1/edit
@@ -38,46 +20,29 @@ class Platform::SolutionsController < ApplicationController
   end
 
   # POST /platform/solutions
-  # POST /platform/solutions.json
   def create
     @platform_solution = Platform::Solution.new(params[:platform_solution])
-
-    respond_to do |format|
-      if @platform_solution.save
-        format.html { redirect_to @platform_solution, notice: 'Solution was successfully created.' }
-        format.json { render json: @platform_solution, status: :created, location: @platform_solution }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @platform_solution.errors, status: :unprocessable_entity }
-      end
+    if @platform_solution.save
+      redirect_to @platform_solution, notice: 'Solution was successfully created.'
+    else
+      redirect_to new_platform_solution_path, notice: 'Problema na criacao'
     end
   end
 
   # PUT /platform/solutions/1
-  # PUT /platform/solutions/1.json
   def update
     @platform_solution = Platform::Solution.find(params[:id])
-
-    respond_to do |format|
-      if @platform_solution.update_attributes(params[:platform_solution])
-        format.html { redirect_to @platform_solution, notice: 'Solution was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @platform_solution.errors, status: :unprocessable_entity }
-      end
+    if @platform_solution.update_attributes(params[:platform_solution])
+      redirect_to @platform_solution, notice: 'Solution was successfully updated.'
+    else
+      redirect_to edit_platform_solution_path, notice: 'Problema na atuazacao'
     end
   end
 
   # DELETE /platform/solutions/1
-  # DELETE /platform/solutions/1.json
   def destroy
     @platform_solution = Platform::Solution.find(params[:id])
     @platform_solution.destroy
-
-    respond_to do |format|
-      format.html { redirect_to platform_solutions_url }
-      format.json { head :no_content }
-    end
+    redirect_to platform_solutions_url
   end
 end
